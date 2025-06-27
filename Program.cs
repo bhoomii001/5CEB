e36cda124e182c7fcd8c045e9d483d5c077088c8
Bhoomi <bhoomiahir321@gmail.conm>
2025-06-27 11:22:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _5CEB
+{
+    internal class Program
+    {
+       public  static void Main(string[] args)
+        {
+            Console.WriteLine("helloo");
+            Console.Write("classss ");
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5CEB
+{
+    internal class sum
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            int sum;
+            sum = a + b;
+            Console.WriteLine("sum is " + sum);
+
+            Console.Read();
+
+        }
+    }
+}
